b5909416c272b2904f9aada0a39f344b0defcc61
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2018-09-14 17:32:36 -0400

    Refactored and added hook into Steam anti-debug code.

--- doorstop/doorstop.cpp
+++ doorstop/doorstop.cpp
@@ -12,57 +12,108 @@
 
 #include "config.h"
 
-IDebugLog               gLog;
-PluginHandle            g_pluginHandle = kPluginHandle_Invalid;
-
 extern "C"
 {
-	__declspec(dllexport) bool F4SEPlugin_Query(const F4SEInterface * f4se, PluginInfo * info)
-	{
-		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Fallout4\\F4SE\\doorstop.log");
+	bool F4SEPlugin_Query(const F4SEInterface *, PluginInfo *);
+	bool F4SEPlugin_Load(const F4SEInterface *);
+}
 
-		_MESSAGE("%s v%s", PLUGIN_NAME_SHORT, PLUGIN_VERSION_STRING);
-		_MESSAGE("%s query", PLUGIN_NAME_SHORT);
+IDebugLog               gLog;
+void * g_moduleHandle;
 
-		// populate info structure
-		info->infoVersion = PluginInfo::kInfoVersion;
-		info->name = PLUGIN_NAME_SHORT;
-		info->version = PLUGIN_VERSION;
+PluginHandle            g_pluginHandle = kPluginHandle_Invalid;
 
-		// store plugin handle so we can identify ourselves later
-		g_pluginHandle = f4se->GetPluginHandle();
+char cbAnnounce[1024];
 
-		// Check game version
-		if (!COMPATIBLE(f4se->runtimeVersion)) {
-			char str[512];
-			sprintf_s(str, sizeof(str), "Your game version: v%d.%d.%d.%d\nExpected version: v%d.%d.%d.%d\n%s will be disabled.",
-				GET_EXE_VERSION_MAJOR(f4se->runtimeVersion),
-				GET_EXE_VERSION_MINOR(f4se->runtimeVersion),
-				GET_EXE_VERSION_BUILD(f4se->runtimeVersion),
-				GET_EXE_VERSION_SUB(f4se->runtimeVersion),
-				GET_EXE_VERSION_MAJOR(SUPPORTED_RUNTIME_VERSION),
-				GET_EXE_VERSION_MINOR(SUPPORTED_RUNTIME_VERSION),
-				GET_EXE_VERSION_BUILD(SUPPORTED_RUNTIME_VERSION),
-				GET_EXE_VERSION_SUB(SUPPORTED_RUNTIME_VERSION),
-				PLUGIN_NAME_LONG
-			);
+void DllStartup(void *address)
+{
+	g_moduleHandle = address;
 
-			MessageBox(NULL, str, PLUGIN_NAME_LONG, MB_OK | MB_ICONEXCLAMATION);
-			return false;
-		}
+	snprintf(cbAnnounce, sizeof(cbAnnounce),
+		PLUGIN_NAME_SHORT " at %p query %p load %p\n", g_moduleHandle,
+		F4SEPlugin_Query, F4SEPlugin_Load);
+}
 
-		if (f4se->runtimeVersion > SUPPORTED_RUNTIME_VERSION) {
-			_MESSAGE("INFO: Newer game version (%08X) than target (%08X).", f4se->runtimeVersion, SUPPORTED_RUNTIME_VERSION);
-		}
+volatile int stopper = 0;
 
-		return true;
+void LogFileInit()
+{
+	gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Fallout4\\F4SE\\doorstop.log");
+
+	_MESSAGE(cbAnnounce);
+}
+
+void AntiDebugHook();
+
+bool Query(const F4SEInterface * f4se, PluginInfo * info)
+{
+	LogFileInit();
+
+	// populate info structure
+	info->infoVersion = PluginInfo::kInfoVersion;
+	info->name = PLUGIN_NAME_SHORT;
+	info->version = PLUGIN_VERSION;
+
+	// store plugin handle so we can identify ourselves later
+	g_pluginHandle = f4se->GetPluginHandle();
+
+	// Check game version
+	if (!COMPATIBLE(f4se->runtimeVersion)) {
+		char str[512];
+		sprintf_s(str, sizeof(str), "Your game version: v%d.%d.%d.%d\nExpected version: v%d.%d.%d.%d\n%s will be disabled.",
+			GET_EXE_VERSION_MAJOR(f4se->runtimeVersion),
+			GET_EXE_VERSION_MINOR(f4se->runtimeVersion),
+			GET_EXE_VERSION_BUILD(f4se->runtimeVersion),
+			GET_EXE_VERSION_SUB(f4se->runtimeVersion),
+			GET_EXE_VERSION_MAJOR(SUPPORTED_RUNTIME_VERSION),
+			GET_EXE_VERSION_MINOR(SUPPORTED_RUNTIME_VERSION),
+			GET_EXE_VERSION_BUILD(SUPPORTED_RUNTIME_VERSION),
+			GET_EXE_VERSION_SUB(SUPPORTED_RUNTIME_VERSION),
+			PLUGIN_NAME_LONG
+		);
 	}
 
-	__declspec(dllexport) bool F4SEPlugin_Load(const F4SEInterface *f4se)
-	{
-		_MESSAGE(PLUGIN_NAME_SHORT " load");
-
-		return true;
+	if (f4se->runtimeVersion > SUPPORTED_RUNTIME_VERSION) {
+		_MESSAGE("INFO: Newer game version (%08X) than target (%08X).", f4se->runtimeVersion, SUPPORTED_RUNTIME_VERSION);
 	}
 
+//	AntiDebugHook();
+
+	return true;
+}
+
+bool Load(const F4SEInterface *f4se)
+{
+	_MESSAGE(PLUGIN_NAME_SHORT " load");
+
+	return true;
+}
+
+/* Exported functions */
+
+extern "C" {
+	bool F4SEPlugin_Query(const F4SEInterface * f4se, PluginInfo * info)
+	{
+		return Query(f4se, info);
+	}
+	
+	bool F4SEPlugin_Load(const F4SEInterface *f4se)
+	{
+		return Load(f4se);
+	}
+
+	BOOL WINAPI DllMain(HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
+	{
+		switch (dwReason)
+		{
+		case DLL_PROCESS_ATTACH:
+			DllStartup((void *)hDllHandle);
+			break;
+
+		case DLL_PROCESS_DETACH:
+			break;
+		};
+
+		return TRUE;
+	}
 };
